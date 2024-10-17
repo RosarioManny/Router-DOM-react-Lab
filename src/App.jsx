e0c429853e2caa03b8fab99301e8649c970ac3c7
RosarioManny <rosario.emm47@gmail.com>
2024-10-17 15:59:24 -0400

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
-// src/App.jsx
+// Functions
+import { Route, Routes } from 'react-router-dom'
+import { useState } from 'react'
+// Data
+
+// Components
+import Navbar from "./components/Navbar.jsx";
+
+// CSS
+import './App.css'
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  
+  return (
+    <>
+    <Navbar />
+    <h1>Hello world!</h1>
+    </>
+
+
+
+  );
 };
 
 export default App;
